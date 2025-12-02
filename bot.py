f1b85f4ce2aad8172a60c42ae5de264738cca031
borjacancelas12-byte <borjacancelas12@gmail.com>
2025-12-02 15:53:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 from handlers import start_command, hoy_command
 
-def create_application(token):8238788823:AAH2Ou1r-QRt-PbofYume0MXCODMrU_MRTE
+def create_application(token):
     app = ApplicationBuilder().token(token).build()
 
     app.add_handler(CommandHandler("start", start_command))
